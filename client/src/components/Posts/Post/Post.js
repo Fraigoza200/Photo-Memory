0ad84d899b8f560dc660833790f6ce79426079ed
Francisco Raigoza <raigoza.frankie@gmail.com>
2020-11-11 11:47:02 -0800

    created first post to MongoDB, Initial App setup

---
+++ client/src/components/Posts/Post/Post.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import useStyles from './styles'
+
+
+function Post() {
+    const classes = useStyles()
+
+    return (
+        <h1>post</h1>
+    )
+}
+
+export default Post
